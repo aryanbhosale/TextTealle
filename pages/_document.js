d3860efaa9fd71218539d1ea2d794ec78bc7d35e
bitsUnxte <f20210274@goa.bits-pilani.ac.in>
2022-12-09 19:39:45 +0530

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
-        <meta charset="UTF-8" />
-        <meta name="description" content="TextTealle is an AI powered blog post generator which uses GPT-3" />
-        <meta name="keywords" content="GPT-3, OpenAI, ChatGPT" />
-        <meta name="author" content="Aryan Bhosale" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="TextTealle" key="title"/>
+        <meta property="og:description" content="TextTealle is an AI powered blog post generator which uses GPT-3" key="description"/>
+        <meta
+          property="og:image"
+          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
         <Main />
